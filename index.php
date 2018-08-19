227734d48a4eb0c008de514e2c2c14653103e0b5
Ilya Sidorchik <ilya@sidorchik.ru>
2018-08-19 23:33:13 +0300

    Auto dev/release mode. Adding of Yandex-services

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
         <title>Поиск книг в библиотеках Москвы</title>
         <meta name="description" content="Более объёмный сводный каталог московских библиотек. Помогает найти книгу в библиотеках Москвы, забронировать и доставить из Некрасовки">
         <meta name="keywords" content="как, где, найти, узнать, проверить, взять, на дом, есть, поиск, нужную, книга, книгу, книг, литература, электронный, единый, сводный, каталог, база, данных, в, библиотека, библиотеках, москва, московских">
-        <!--<meta name="yandex-verification" content="835e608657377f1e" />-->
         <meta content="width=device-width, initial-scale=1" name="viewport">
         <link rel="shortcut icon" href="/img/favicon.ico">
         <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon-180x180.png">
@@ -16,7 +15,7 @@
             <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.min.js"></script>
             <script src="https://raw.githubusercontent.com/jonathantneal/flexibility/master/flexibility.js"></script>
         <![endif]-->
-        <!-- Yandex.Metrika counter --> <script> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter49510096 = new Ya.Metrika2({ id:49510096, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/49510096" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+        <?php if ($_SERVER['SERVER_NAME'] == 'booksearcher.ru') echo '<meta name="yandex-verification" content="835e608657377f1e"/><!-- Yandex.Metrika counter --> <script> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter49510096 = new Ya.Metrika2({ id:49510096, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/49510096" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->'; ?>
     </head>
     <body>
         <main class="mt-4">
